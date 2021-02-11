33735dfcc883a84700e0c6f256f837c430d17cd2
harshgeek4coder <sharsh2222@gmail.com>
2021-02-11 21:36:47 +0530

    Custom Changes-modify main to run on colab

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 """
 Main Driver Code
+Modyfying TO run on COLAB - PARAMS
 
 """
 
@@ -8,9 +9,9 @@ from ML.train import train
 import matplotlib.pyplot as plt
 
 
-def main():
-    modeltrain = train(path='../fashiondata/img', epochs=1, batch_size=1,
-                     vec_shape=100, split=[20,1,0], noisedim=100)
+def main(g_path,n_epochs):
+    modeltrain = train(path=g_path, epochs=n_epochs, batch_size=4,
+                     vec_shape=100, split=[1,2000,0], noisedim=100)
     modeltrain.trainer()
 
 if __name__ == "__main__":
