fa660e8c4f824b1ddf3a44984bfca3c25e145216
Aditya Shukla <adityashukzy@gmail.com>
2021-02-11 13:47:52 +0530

    Fix(main): Add variables for path and epochs

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ import matplotlib.pyplot as plt
 from time import time
 
 
-def main():
-    modeltrain = train(path="Data", epochs=1, batch_size=4, vec_shape=100, split=[1, 2000, 0], noisedim=100)
+def main(inpPath, inpEpochs):
+    modeltrain = train(path=inpPath, epochs=inpEpochs, batch_size=4, vec_shape=100, split=[1, 2000, 0], noisedim=100)
     starting = time()
     modeltrain.trainer()
     print(time() - starting)
