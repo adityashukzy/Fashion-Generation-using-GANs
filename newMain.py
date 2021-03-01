2ccc678c02d15e07688349b7a3fd25fe54217e79
Aditya Shukla <adityashukzy@gmail.com>
2021-03-01 15:53:47 +0530

    Fix(main): Rewrite main as separate file

---
+++ newMain.py
@@ -0,0 +1,18 @@
+"""
+Main Driver Code
+"""
+
+from ML.newTrain import Train
+import matplotlib.pyplot as plt
+from time import time
+
+def main(inp_path, inp_epochs):
+	model_train = Train(path=inp_path, epochs=inp_epochs, batch_size=4, split=[], noise_dim=100)
+	starting = time()
+	
+	model_train.trainer()
+	print(time() - starting)
+	model_train.plot_trainer()
+
+if __name__ == "__main__":
+	main()
