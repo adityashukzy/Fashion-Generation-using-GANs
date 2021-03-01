455c6d7c841ad5797d8ab96b0fe377a18be9a0e4
Aditya Shukla <adityashukzy@gmail.com>
2021-03-01 15:59:37 +0530

    Fix(newMain): Make separate input variable for split while training

--- newMain.py
+++ newMain.py
@@ -6,8 +6,8 @@ from ML.newTrain import Train
 import matplotlib.pyplot as plt
 from time import time
 
-def main(inp_path, inp_epochs):
-	model_train = Train(path=inp_path, epochs=inp_epochs, batch_size=4, split=[], noise_dim=100)
+def main(inp_path, inp_epochs, inp_split):
+	model_train = Train(path=inp_path, epochs=inp_epochs, batch_size=4, split=inp_split, noise_dim=100)
 	starting = time()
 	
 	model_train.trainer()
